442d9023327c6b9ea82d634883f2663cd92f51bb
debugkei <ivankei0009@gmail.com>
2025-04-24 17:14:15 -0700

    Finished sortedVector implementation

---
+++ src/structs/sortedVector/sortedVector.c
@@ -0,0 +1,162 @@
+#include"sortedVector.h"
+
+#include<stdlib.h>
+#include<assert.h>
+#include<string.h>
+#include<malloc.h>
+
+static void _DefaultDest(void* pV){}
+
+///@brief This function slides a part between start and middle pointers to the end, and end slides left.
+///@param pStart is the starting point, which will be filled with middle values.
+///@param pMiddle is the middle point all starting from it will be sliden to the left to starting point.
+///@param pEnd is the ending point, pStart - pMiddle will be now at the very end.
+static void Slide(void* pStart, void* pMiddle, void* pEnd){
+  int nFirst = pMiddle - pStart;
+  int nSecond = pEnd - pMiddle;
+  char buffer[nFirst];
+
+  memcpy(buffer, pStart, nFirst);
+  memmove(pStart, pMiddle, nSecond);
+  void* pNewEnd = (char*)pStart + nSecond;
+  memcpy(pNewEnd, buffer, nFirst);
+}
+
+///@brief Expands the vector's allocated space.
+static void ExpandVector(SortedVector* pVec){
+  assert(pVec != NULL);
+
+  //Just double the vector's allocated space
+  int newSize = pVec->_allocLength * 2;
+
+  pVec->_elems = realloc(pVec->_elems, newSize);
+  assert(pVec->_elems != NULL);
+
+  pVec->_allocLength = newSize;
+}
+
+///@brief Utilizes binary search to find a place to insert an element to
+///@param pElems is the array at which at every element theres a priority measure, represented by offset and int.
+///@param pElem is the element itself that has to be inserted
+///@param elemSize is the size of a single element within the array.
+///@param n is the length of pElems.
+static int BSearchForInsertPos(void* pElems, void* pElem, int elemSize, int n, _CmpFn cmpFn){
+  int left = 0, right = n - 1;
+
+  while (left <= right){
+    int middle = left + (right - left) / 2;
+    void* middleElem = pElems + middle * elemSize;
+    int cmpRes = cmpFn(pElem, middleElem);
+
+    if (cmpRes > 0){
+      //pElem > middleElem
+      left = middle + 1;
+    } else if (cmpRes < 0){
+      //pElem < middleElem
+      right = middle - 1;
+    } else{
+      return middle;
+    }
+  }
+
+  return left;
+}
+
+int _IntCmp(void* pV1, void* pV2){
+  int i1 = *(int*)pV1;
+  int i2 = *(int*)pV2;
+
+  return i1 - i2;
+}
+
+void InitSortedVector(SortedVector* pVec, int elemSize, _DestFn destFn, _CmpFn cmpFn){
+  assert(pVec != NULL);
+
+  pVec->length = 0;
+  pVec->_allocLength = 4;
+  pVec->_elemSize = elemSize; 
+
+  pVec->_elems = malloc(pVec->_elemSize * pVec->_allocLength);
+  assert(pVec->_elems != NULL);
+
+  if (destFn != NULL){
+    pVec->_destFn = destFn;
+  } else{
+    pVec->_destFn = _DefaultDest;
+  }
+
+  if (cmpFn != NULL){
+    pVec->_cmpFn = cmpFn;
+  } else{
+    pVec->_cmpFn = _IntCmp;
+  }
+}
+
+void* GetElementAtSortedVector(SortedVector* pVec, int index){
+  assert(pVec->length > index);
+  assert(pVec->length != 0);
+  assert(pVec != NULL);
+
+  void* pElem = pVec->_elems + index * pVec->_elemSize;
+  return pElem;
+}
+
+void PopElementAtSortedVector(SortedVector* pVec, int index){
+  assert(pVec->length > index);
+  assert(pVec->length != 0);
+  assert(pVec != NULL);
+
+  int elemSize = pVec->_elemSize;
+  char* elems = pVec->_elems;
+  int n = pVec->length;
+
+  //Slide the element to pop to the back
+  void* pElemStart = elems + index * elemSize;
+  void* pElemEnd = elems + (index + 1) * elemSize;
+  void* pElemsEnd = elems + n * elemSize;
+  Slide(pElemStart, pElemEnd, pElemsEnd);
+
+  //Destroy that element accordingly to destFn
+  void* pElem = GetElementAtSortedVector(pVec, n - 1);
+  pVec->_destFn(pElem);
+
+  --pVec->length;
+}
+
+void PushElementSortedVector(SortedVector* pVec, void* pElem){
+  assert(pVec != NULL);
+
+  int n = pVec->length;
+
+  //Expand if needed
+  if (n >= pVec->_allocLength){
+    ExpandVector(pVec);
+    n = pVec->length;
+  }
+
+  int elemSize = pVec->_elemSize;
+
+  //Insert at specific place
+  int insertAt = BSearchForInsertPos(pVec->_elems, pElem, elemSize, n, pVec->_cmpFn);
+
+  void* pElemStart = (char*)pVec->_elems + insertAt * elemSize;
+  void* pElemsEnd = (char*)pVec->_elems + (n - 1) * elemSize;
+  void* pElemAfterEnd = pElemsEnd + elemSize;
+  Slide(pElemStart, pElemsEnd, pElemAfterEnd);
+
+  memcpy(pElemStart, pElem, elemSize);
+
+  ++pVec->length;
+}
+
+void ClearSortedVector(SortedVector* pVec){
+  int n = pVec->length;
+  for (int i = 0; i < n; ++i){
+    PopElementAtSortedVector(pVec, 0);
+  }
+}
+
+void DestroySortedVector(SortedVector* pVec){
+  ClearSortedVector(pVec);
+  free(pVec->_elems);
+}
--- src/structs/sortedVector/sortedVector.h
+++ src/structs/sortedVector/sortedVector.h
@@ -1,30 +1,42 @@
 ///Just a function signature that is called on destruction.
 typedef void(*_DestFn)(void*);
 
+///Just a function signature that is called to compare 2 elements.
+typedef int(*_CmpFn)(void*, void*);
+
 ///SortedVector data structure.
 ///Look in sortedVector.h for more information on existing functions.
 ///Sorted least->greatest.
 ///Can either contain structs with priority int at specific offset, or ints with offset of 0.
 typedef struct{
-  int _offsetOfSortElem;
   char* _elems;
   int length;
   int _allocLength;
   _DestFn _destFn;
+  int _elemSize;
+  _CmpFn _cmpFn;
 } SortedVector;
 
 ///@brief Initializes a SortedVectorInstance to be ready to serve.
 ///@param pVec is a pointer to a SortedVector struct created.
-///@param sortedElementOffset is the offset of the int value in bytes, that is going to be used to sort (least->greatest).
-///The element has to be a struct or an int.
 ///@param elemSize is the size of a single element stored in the SortedVector in bytes.
 ///@param destFn is the function that is used to destroy a single element, it takes a void pointer to an element to destroy.
 ///If the values are not pointers just pass in null.
-void InitSortedVector(SortedVector* pVec, int sortedElementOffset, int elemSize, _DestFn destFn);
+///@param _cmpFn is the function that compares 2 elements, this way the vector is sorted.
+///It returns an int, where -1 means that first value passed is less than second,
+///0 means they are equal,
+///and 1 means that first value is greater.
+///It takes 2 void*.
+///If NULL passed defaults to int compare.
+void InitSortedVector(SortedVector* pVec, int elemSize, _DestFn destFn, _CmpFn cmpFn);
 
 ///@brief Destroys the SortedVector and clears the space occupied by it.
 ///@param pVec is a pointer to pVector to destroy, after this function the pVector can be initalized with InitSortedVector.
 void DestroySortedVector(SortedVector* pVec);
+
+///@brief Clears a sorted vector from all the elements.
+///@param pVec is a pointer to SortedVector to clear.
+void ClearSortedVector(SortedVector* pVec);
 
 ///@brief Gets an element and returns a pointer to it at specific index.
 void* GetElementAtSortedVector(SortedVector* pVec, int index);
@@ -33,9 +45,9 @@ void* GetElementAtSortedVector(SortedVector* pVec, int index);
 ///@param pVec is the SortedVector from which element is popped.
 ///@param index is the index at which the element has to be popped.
 ///@param elemBasket is a pointer to a pointer of preallocated space to which the element will be copied.
-void PopElementAtSortedVector(SortedVector* pVec, int index, void** elemBasket);
+void PopElementAtSortedVector(SortedVector* pVec, int index);
 
 ///@brief This function pushes an element onto SortedVector, and orders it in the right order, least->greatest.
 ///@param pVec is the vector that the element is pushed onto.
 ///@param elem is a pointer to an element which will be copied and put onto the vector.
-void PushElementSortedVector(SortedVector* pVec, void* elem);
+void PushElementSortedVector(SortedVector* pVec, void* pElem);
